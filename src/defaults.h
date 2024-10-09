106a4f7b0abc4edd861507b99820a839b4fffead
SQ6NTI <miskrzyn@gmail.com>
2024-10-09 20:56:33 +0200

    general configuration base and defaults improvements

--- src/defaults.h
+++ src/defaults.h
@@ -1,2 +1,11 @@
 #pragma once
 
+#define DEFAULT_LORA_FREQ 433.775       /* Default LoRa frequency in MHz */
+#define DEFAULT_LORA_BW 125             /* Default LoRa bandwidth in kHz */
+#define DEFAULT_LORA_SF 9               /* Default LoRa spreading factor (SF) */
+#define DEFAULT_LORA_CR 7               /* Default LoRa coding rate (CR) */
+#define DEFAULT_LORA_SYNCWORD 0x12      /* Default LoRa sync word (RADIOLIB_SX126X_SYNC_WORD_PRIVATE for now) */
+#define DEFAULT_LORA_POWER 10           /* Default LoRa power in dBm */
+#define DEFAULT_LORA_PREAMBLE_LEN 8     /* Default LoRa preamble length in symbols */
+#define DEFAULT_LORA_TCXO_V 1.8         /* Default LoRa preamble length in symbols */
+
